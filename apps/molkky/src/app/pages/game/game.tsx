1c46700321c3ab33402baab0277b2e7adece2312
Thomas Legrand <legrand.thomas5@hotmail.fr>
2022-07-27 22:07:51 +0200

    clean imports

--- apps/molkky/src/app/pages/game/game.tsx
+++ apps/molkky/src/app/pages/game/game.tsx
@@ -2,19 +2,10 @@ import PinGroup from '../../components/pin-group/pin-group';
 import useThrow from '../../hooks/use-throw/use-throw';
 import styles from './game.module.css';
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import {
-  gameActions,
-  Player,
-  selectPlayers,
-  selectThrows,
-  selectWinner,
-  Throw,
-} from '../../store/game.slice';
 import useGameStart from '../../hooks/use-game-start/use-game-start';
+import { Player, selectPlayers, selectWinner } from '../../store/game.slice';
 
 /* eslint-disable-next-line */
 export interface GameProps {}
@@ -23,7 +14,6 @@ export function Game(props: GameProps) {
   const { onClickRestart, onClickReset } = useGameStart();
   const { currentPlayer, onPinClinkHandle, onUndoThrow } = useThrow();
   const winner = useSelector(selectWinner);
-  const throws = useSelector(selectThrows);
   const players = useSelector(selectPlayers);
 
   return (
